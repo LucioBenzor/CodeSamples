3c47b3f68815c1c24be42b03f1f437cec2db06df
Lucio Benzor <lbenzor1@gmail.com>
2023-08-02 14:44:12 -0400

    added functions to exclude domains
    Note the excluded domains are uga.edu, maimi.edu and gatech.edu

--- Add-DLPSenderExclusions.ps1
+++ Add-DLPSenderExclusions.ps1
@@ -1,3 +1,4 @@
+
 # Install required module if not already installed
 if (!(Get-Module -Name ExchangeOnlineManagement -ErrorAction SilentlyContinue)) {
     Install-Module -Name ExchangeOnlineManagement -Force -AllowClobber
@@ -10,17 +11,28 @@ Connect-IPPSSession
 
 # Function to get the Compliance Policies from DLP
 function Get-DLPCompliancePolicies {
-    $compliancePolicies = Get-DlpCompliancePolicy
-    return $compliancePolicies
+    Get-DlpCompliancePolicy
 }
 
-# Main script to iterate through Compliance Policies
+# Function to add domain exclusions to a DLP policy
+function Add-DLPDomainExclusions {
+    param (
+        [string]$PolicyId,
+        [string[]]$Domains
+    )
+    foreach ($domain in $Domains) {
+        Set-DlpComplianceRule -Identity $PolicyId -ExceptIfSenderDomainIs $domain
+        Write-Host "Exclusion added for domain: $domain"
+    }
+}
+
+# Main script to iterate through Compliance Policies and add domain exclusions
 try {
     # Get all Compliance Policies
     $policies = Get-DLPCompliancePolicies
 
     if ($policies) {
-        # Iterate through each policy and display information
+        # Iterate through each policy
         foreach ($policy in $policies) {
             $policyName = $policy.Name
             $policyId = $policy.Id
@@ -29,6 +41,12 @@ try {
             Write-Host "Policy Name: $policyName"
             Write-Host "Policy ID: $policyId"
             Write-Host "Policy State: $policyState"
+
+            # Add domain exclusions for each policy
+            $domainsToAdd = @("uga.edu", "miami.edu", "gatech.edu")
+            Add-DLPDomainExclusions -PolicyId $policyId -Domains $domainsToAdd
+
+            Write-Host "Exclusions added for uga.edu, miami.edu, gatech.edu"
             Write-Host "------------------------"
         }
     } else {
